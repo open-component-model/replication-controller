de89424405c0a0172ed46af3eda666296efcf42d
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-04-13 14:28:31 +0200

    added unit test for authentication

--- pkg/ocm/ocm_test.go
+++ pkg/ocm/ocm_test.go
@@ -8,13 +8,17 @@ import (
 
 	_ "github.com/distribution/distribution/v3/registry/storage/driver/inmemory"
 	"github.com/fluxcd/pkg/apis/meta"
-	"github.com/open-component-model/ocm/pkg/contexts/ocm"
-	"github.com/open-component-model/replication-controller/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/open-component-model/ocm/pkg/contexts/credentials/cpi"
+	"github.com/open-component-model/ocm/pkg/contexts/oci/identity"
+	"github.com/open-component-model/ocm/pkg/contexts/ocm"
+
+	"github.com/open-component-model/replication-controller/api/v1alpha1"
 )
 
 func TestClient_GetComponentVersion(t *testing.T) {
@@ -114,7 +118,7 @@ func TestClient_GetComponentVersion(t *testing.T) {
     "ghcr.io": {
       "username": "skarlso",
       "password": "password",
-      "auth": "cGhvYmFuMDE6YmxhaA=="
+      "auth": "c2thcmxzbzpwYXNzd29yZAo="
     }
   }
 }`),
@@ -151,6 +155,79 @@ func TestClient_GetComponentVersion(t *testing.T) {
 			assert.Equal(t, cs.Spec.Component, cva.GetName())
 		})
 	}
+}
+
+func TestClient_CreateAuthenticatedOCMContext(t *testing.T) {
+	cs := &v1alpha1.ComponentSubscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-name",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.ComponentSubscriptionSpec{
+			Component: "github.com/skarlso/ocm-demo-index",
+			Semver:    ">v0.0.1",
+			Source: v1alpha1.OCMRepository{
+				URL: env.repositoryURL,
+			},
+			ServiceAccountName: "test-service-account",
+		},
+	}
+	serviceAccount := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-service-account",
+			Namespace: "default",
+		},
+		ImagePullSecrets: []corev1.LocalObjectReference{
+			{
+				Name: "test-name-secret",
+			},
+		},
+	}
+	testSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-name-secret",
+			Namespace: "default",
+		},
+		Data: map[string][]byte{
+			".dockerconfigjson": []byte(`{
+  "auths": {
+    "ghcr.io": {
+      "username": "skarlso",
+      "password": "password",
+      "auth": "c2thcmxzbzpwYXNzd29yZAo="
+    }
+  }
+}`),
+		},
+		Type: corev1.SecretTypeDockerConfigJson,
+	}
+
+	fakeKubeClient := env.FakeKubeClient(WithObjets(cs, serviceAccount, testSecret))
+	ocmClient := NewClient(fakeKubeClient)
+	component := "github.com/skarlso/ocm-demo-index"
+
+	err := env.AddComponentVersionToRepository(Component{
+		Name:    component,
+		Version: "v0.0.1",
+	})
+	require.NoError(t, err)
+
+	octx, err := ocmClient.CreateAuthenticatedOCMContext(context.Background(), cs)
+	require.NoError(t, err)
+
+	id := cpi.ConsumerIdentity{
+		identity.ID_TYPE:       identity.CONSUMER_TYPE,
+		identity.ID_HOSTNAME:   "ghcr.io",
+		identity.ID_PATHPREFIX: "skarlso",
+	}
+	creds, err := octx.CredentialsContext().GetCredentialsForConsumer(id)
+	require.NoError(t, err)
+	consumer, err := creds.Credentials(nil)
+	require.NoError(t, err)
+
+	assert.Equal(t, "password", consumer.Properties()["password"])
+	assert.Equal(t, "skarlso", consumer.Properties()["username"])
+	assert.Equal(t, "ghcr.io", consumer.Properties()["serverAddress"])
 }
 
 func TestClient_GetLatestValidComponentVersion(t *testing.T) {
